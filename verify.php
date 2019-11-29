ed707cc3297014fffcf2e6161930e23749ac31e0
BreeZaRA <50435194+BreeZaRA@users.noreply.github.com>
2019-11-29 11:10:28 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php 
-  $access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+  $access_token = 'C+8IIYep0Mtvq7TE7Ubo3aR6A3hZsXXnY9GDH37347aFaLRIjplTAvzpp32QK+mT56zbBhpkxT2nngdmAvUcPhYhF7Unve25RRohyRjyTBS1dbOqY87c6TcPRGW/2Z91hjFY/mm+FCB01Ykh10atewdB04t89/1O/w1cDnyilFU=';
   $url = 'https://api.line.me/v1/oauth/verify';
   $headers = array('Authorization: Bearer ' . $access_token);
   $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
